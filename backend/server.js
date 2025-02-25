5e2fcdd551486969bad9976ac16cefc967e96eb9
hung <hung2k3vn@gmail.com>
2025-02-25 09:29:01 +0700

    admin, seller login

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use('/api', require('./routes/authRoutes'))
  
 const port = process.env.PORT
 dbConnect()
