6ad12a37866059085650aeec56ae9d4eaa647fa8
hung <hung2k3vn@gmail.com>
2025-02-25 09:27:08 +0700

    add middleware

---
+++ backend/middlewares/adminMiddleware.js
@@ -0,0 +1,19 @@
+const adminModel = require("../models/adminModel");
+const sellerModel = require("../models/sellerModel");
+const authenticateToken = require("./authenticateToken");
+
+const systemMiddleware = async (req, res, next) => {
+  authenticateToken(req, res, async () => {
+    const allowedRoles = ['admin'];
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied, only admin allowed' });
+    }
+    const model = req.user.role === 'admin' ? adminModel : sellerModel;
+    const user = await model.findById(req.user.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    req.user = { id: user._id, role: user.role };
+    next();
+  });
+};
+
+module.exports = systemMiddleware;
---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async(req, res, next) =>{
+    const {accessToken} = req.cookies
+
+    if (!accessToken) {
+        return res.status(409).json({ error : 'Please Login First'})
+    } else {
+        try {
+            const deCodeToken = await jwt.verify(accessToken,process.env.SECRET)
+            req.role = deCodeToken.role
+            req.id = deCodeToken.id
+            next()            
+        } catch (error) {
+            return res.status(409).json({ error : 'Please Login'})
+        }        
+    }
+
+}
+
+module.exports = authMiddleware
