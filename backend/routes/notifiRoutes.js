429f4832a8299f9236a3e402a8cfdd205e70f7e7
mtuan64 <tuanodaynay@gmail.com>
2025-03-14 14:29:24 +0700

    fix routers after update

--- backend/routes/notifiRoutes.js
+++ backend/routes/notifiRoutes.js
@@ -6,12 +6,12 @@ const notificationController = require("../controllers/notifiController");
 router.post("/notify/create", notificationController.createNotification);
 
 // Get notifications for a seller
-router.get("/notify/:sellerId", notificationController.getNotifications);
+router.get("/notify", notificationController.getNotifications);
 
 // Mark notification as read
-router.put("/notify/:id", notificationController.markAsRead);
+router.put("/notify/read/:id", notificationController.markAsRead);
 
 // Delete a notification
-router.delete("/notify/:id", notificationController.deleteNotification);
+router.delete("/notify/delete/:id", notificationController.deleteNotification);
 
 module.exports = router;
