be4c8fd7efe363a800249b94094bc8ab0d234c17
mtuan64 <tuanodaynay@gmail.com>
2025-03-14 14:31:42 +0700

    fix routers after update

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,7 +5,7 @@ router.post("/chat/customer/send-message-to-seller", ChatController.customer_mes
 router.post("/chat/send-message", ChatController.message_add);
 router.post("/chat/admin/send-message", ChatController.admin_message_insert);
 
-router.get("/chat/get-messages/:userId/:otherUserId", ChatController.get_messages);
-router.get("/chat/get-chat-participants/:userId", ChatController.get_chat_participants);
+router.get("/chat/get-messages/:otherUserId", ChatController.get_messages);
+router.get("/chat/get-chat-participants", ChatController.get_chat_participants);
 
 module.exports = router;
