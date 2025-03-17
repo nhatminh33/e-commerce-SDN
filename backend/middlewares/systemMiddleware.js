cc2d43a7df2ff030bc7366730e35e1e4be28ab54
mtuan64 <tuanodaynay@gmail.com>
2025-03-17 09:49:40 +0700

    small fix middlewares

--- backend/middlewares/systemMiddleware.js
+++ backend/middlewares/systemMiddleware.js
@@ -2,15 +2,15 @@
 const authenticateToken = require("./authenticateToken");
 const userModel = require("../models/userModel");
 
-const systemMiddleware = async (req, res, next) => {
+const   systemMiddleware = async (req, res, next) => {
     authenticateToken(req, res, async () => {
         try {
             const allowedRoles = ["admin", "seller"];
-            if (!allowedRoles.includes(req.user.role)) {
+            if (!allowedRoles.includes(req.role)) {
                 return res.status(403).json({ message: "Access denied, only admin or seller allowed" });
             }
 
-            const user = await userModel.findById(req.user.id).select("_id role");
+            const user = await userModel.findById(req.id).select("_id role");
             if (!user) {
                 return res.status(404).json({ message: "User not found" });
             }
